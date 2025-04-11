256abeccc39cd82a81323d14832b8f3036991bc3
durgadevialla <durgadevialla080103@gmail.com>
2025-04-11 21:50:17 +0530

    Update Login.jsx

--- client/src/Login.jsx
+++ client/src/Login.jsx
@@ -43,7 +43,7 @@ const Login = ({ onLogin, onRegisterClick, onBack }) => {
     setServerError('');
 
     try {
-      const response = await fetch('http://localhost:5000/api/auth/login', {
+      const response = await fetch('https://goods-grid-1.onrender.com/api/auth/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData)
